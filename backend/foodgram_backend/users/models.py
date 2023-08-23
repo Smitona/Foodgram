4de42ca91aafd0f31fc5fc5613d934c5eb2f321e
Smitona <jarofsmetana@yandex.ru>
2023-08-23 15:42:27 +0300

    App users started

--- backend/foodgram_backend/users/models.py
+++ backend/foodgram_backend/users/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        required=True,
+        validators=[
+            RegexValidator(r'^[\w.@+-]+\z'),
+        ],
+    )
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        required=True,
+    )
+    first_name = models.CharField(
+        max_length=150,
+        unique=True,
+        required=True,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        unique=True,
+        required=True,
+    )
+    password = models.CharField(
+        max_length=150,
+        required=True,
+        validators=[
+            RegexValidator(r'^[\w.@+-]+\z'),
+        ],
+    )
+
+    class Meta:
+        ordering = ('username',)
+    
+    def __str__(self) -> str:
+        return self.username
+
+
