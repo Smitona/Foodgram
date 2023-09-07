a5c641495de1f13d2930dcc7e979a0cd984365ef
Anna Plastinina <e.tipsin@mail.ru>
2023-09-07 17:46:42 +0300

    Fixed

--- backend/foodgram_backend/users/urls.py
+++ backend/foodgram_backend/users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from users.views import UserViewSet, SubscribtionsViewSet
+from users.views import SubscribtionsViewSet #, UserViewSet
 
 router = routers.DefaultRouter()
 
-router.register('users', UserViewSet)
+# router.register('users', UserViewSet)
 router.register('users/subsciptions', SubscribtionsViewSet)
 router.register(
     r'users/(?P<user_id>\d+)/subscribe', SubscribtionsViewSet
@@ -15,6 +15,7 @@ app_name = 'users'
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    path('', include('djoser.urls')),
+
 ]
--- backend/foodgram_backend/users/views.py
+++ backend/foodgram_backend/users/views.py
@@ -1,10 +1,10 @@
-from django.db import IntegrityError
+# from django.db import IntegrityError
 
 from rest_framework import serializers, status, viewsets
-from rest_framework.decorators import action
-from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.response import Response
+# from rest_framework.decorators import action
+# from rest_framework.views import APIView
+# from rest_framework.permissions import AllowAny, IsAuthenticated
+# from rest_framework.response import Response
 
 from users.models import CustomUser, UserFollower
 from users.serializers import (
@@ -12,51 +12,51 @@ from users.serializers import (
 )
 
 
-class SignUpView(APIView):
-    permissions_classes = (AllowAny,)
-    serializer_class = UserCreateSerializer
+# class SignUpView(APIView):
+#     permissions_classes = (AllowAny,)
+#     serializer_class = UserCreateSerializer
+#
+#     def post(self, request):
+#         serializer = self.serializer_class(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         try:
+#             CustomUser.objects.get_or_create(
+#                 email=serializer.validated_data.get('email'),
+#                 username=serializer.validated_data.get('username'),
+#             )
+#         except IntegrityError:
+#             raise serializers.ValidationError(
+#                 'Пользователь с такой почтой уже зарегистрирован.',
+#             )
+#         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        try:
-            CustomUser.objects.get_or_create(
-                email=serializer.validated_data.get('email'),
-                username=serializer.validated_data.get('username'),
-            )
-        except IntegrityError:
-            raise serializers.ValidationError(
-                'Пользователь с такой почтой уже зарегистрирован.',
-            )
-        return Response(serializer.data, status=status.HTTP_200_OK)
 
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
-
-    @action(
-        methods=['get', 'patch'],
-        detail=False,
-        url_path='me', url_name='me',
-        permission_classes=(IsAuthenticated,),
-        serializer_class=UserMeSerializer,
-    )
-    def me(self, request, *args, **kwargs):
-        if request.method == 'GET':
-            user = self.request.user
-            serializer = self.get_serializer(user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        if request.method == 'PATCH':
-            serializer = self.get_serializer(
-                request.user,
-                data=request.data,
-                partial=True,
-            )
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = CustomUser.objects.all()
+#     serializer_class = UserSerializer
+#
+#     @action(
+#         methods=['get', 'patch'],
+#         detail=False,
+#         url_path='me', url_name='me',
+#         permission_classes=(IsAuthenticated,),
+#         serializer_class=UserMeSerializer,
+#     )
+#     def me(self, request, *args, **kwargs):
+#         if request.method == 'GET':
+#             user = self.request.user
+#             serializer = self.get_serializer(user)
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#
+#         if request.method == 'PATCH':
+#             serializer = self.get_serializer(
+#                 request.user,
+#                 data=request.data,
+#                 partial=True,
+#             )
+#             serializer.is_valid(raise_exception=True)
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class SubscribtionsViewSet(viewsets.ModelViewSet):
@@ -64,6 +64,6 @@ class SubscribtionsViewSet(viewsets.ModelViewSet):
     serializer_class = SubsctibeSerilizer
 
 
-class TokenView(APIView):
-    permissions_classes = (AllowAny,)
-    serializer_class = UserCreateSerializer
+# class TokenView(APIView):
+#     permissions_classes = (AllowAny,)
+#     serializer_class = UserCreateSerializer
